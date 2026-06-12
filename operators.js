5c0d3f29b6162ed67921a6737c735a0b38bb87d0
ABINAYAGOPINATH14 <abinayag25csa@vetias.ac.in>
2026-06-12 11:15:06 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,9 @@
+//addition
+ var a = 10
+ var b= 20
+ console.log(a+b) 
+ console.log(a-b) //subtraction
+ console.log(a*b) //multiplication
+ console.log(a/b) //divition
+ console.log(a%b) //modulus
+ console.log(a**b) //exponentional
