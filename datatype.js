9f08086b0a4a7bc9e51a87d09831e01df06797b1
ABINAYAGOPINATH14 <abinayag25csa@vetias.ac.in>
2026-06-12 14:52:22 +0530

    datatypes

---
+++ datatype.js
@@ -0,0 +1,45 @@
+//datatypes
+console.log("DATA TYPES")
+
+//numbers
+var a = 10
+console.log(a)
+
+//string
+var name = "John"
+console.log(name)
+
+//boolean
+var isStudent = true
+console.log(isStudent) 
+
+//null
+var b = null
+console.log(b)
+
+//undefined
+var c
+console.log(c)
+
+//bigint
+var d = 1234567890123456789012345678901234567890n
+console.log(d)
+
+//symbol
+var e = Symbol("symbol")
+console.log(e)
+
+//non-primitive data types
+//array
+var arr = [1, 2, 3, 4, 5]
+console.log(arr)
+
+
+//object
+var person = {
+    name: "John",
+    age: 30,
+    isStudent: true
+}
+console.log(person)     
+
