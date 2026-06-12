e7ed4d8e1324b43ea0c841087372d750479bb0b3
ABINAYAGOPINATH14 <abinayag25csa@vetias.ac.in>
2026-06-12 11:37:50 +0530

    comparison

---
+++ comparison.js
@@ -0,0 +1,8 @@
+//comparison
+var a = 20
+var b = 30
+console.log(a==b) //equal to
+console.log(a!=b) //not equal to
+console.log(a<=b) //less than or equal to
+console.log(a>=b) //greater than or equal to
+
