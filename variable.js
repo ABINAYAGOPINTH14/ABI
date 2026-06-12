45087c28832861562130decb94d4da4f9535e12e
ABINAYAGOPINATH14 <abinayag25csa@vetias.ac.in>
2026-06-12 10:20:58 +0530

    variables

---
+++ variable.js
@@ -0,0 +1,13 @@
+// variable declaration using var
+var name = " VET IAS "
+console.log(name)
+
+// variable declaration usin let
+{
+    let age = " 20"
+    console.log(age)
+}
+
+// variable declaration using const
+const country = " INDIA"
+console.log (country)
